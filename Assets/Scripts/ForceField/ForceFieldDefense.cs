fe1cf7ee917e50b9fa4d4e91470a60e9b6cddda2
hjfritz <fritzhj@gmail.com>
2022-10-05 10:46:22 -0700

    Smoothed out defense action

--- Assets/Scripts/ForceField/ForceFieldDefense.cs
+++ Assets/Scripts/ForceField/ForceFieldDefense.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ForceField
 {
@@ -6,27 +7,64 @@ namespace ForceField
     {
         
         [SerializeField] private GameObject forceField;
+        [SerializeField] private InputActionReference start;
         
         public static int fieldStrength = 0;
+        
         private Vector3 _ffScale = new Vector3(.1f, .1f, .01f);
+        private bool counting = false;
+        private float defenseDuration = 20.0f;
+        private float defenseTimer = 0f;
         
         // Start is called before the first frame update
         new void Start()
         {
             forceField.transform.localScale = _ffScale;
+            
+            base.Start();
+
+            start.action.performed += TestGame;
+        }
+
+        private void TestGame(InputAction.CallbackContext obj)
+        {
+            ExecuteAction();
+        }
+
+        public override void ExecuteAction()
+        {
+            counting = true;
+            defenseTimer = defenseDuration;
+            base.ExecuteAction();
         }
 
         // Update is called once per frame
         void Update()
         {
-            _ffScale = new Vector3(.01f * fieldStrength, .01f * fieldStrength, .01f);
-
-            forceField.transform.localScale = _ffScale;
+            
             
             if (fieldStrength >= 100)
             {
-                Success();
+                FinalizeAction();
+            }else if (defenseTimer <= 0)
+            {
+                FinalizeAction();
             }
+            else
+            {
+                defenseTimer -= Time.deltaTime;
+                
+                _ffScale = new Vector3(.01f * fieldStrength, .01f * fieldStrength, .01f);
+
+                forceField.transform.localScale = _ffScale;
+            }
+        }
+        
+        public override void FinalizeAction()
+        {
+            AbilityComplete.Invoke(fieldStrength);
+            Debug.Log("Success!");
+            ResetAbility();
         }
         
         public void AddFieldStrength(float handSpeed)
@@ -34,10 +72,11 @@ namespace ForceField
             fieldStrength += (int)handSpeed;
         }
         
-        private void Success()
+        private void ResetAbility()
         {
-            Debug.Log("Success!");
-            AbilityComplete.Invoke(fieldStrength);
+            counting = false;
+            defenseTimer = defenseDuration;
+            fieldStrength = 0;
         }
     }
 }
