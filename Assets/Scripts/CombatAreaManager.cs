8373f6a0054b6ef952fec3be066045cd4d52b66a
Sikaar <sikaar@gmail.com>
2022-10-13 20:48:36 +0200

    Working on the separated player manager

---
+++ Assets/Scripts/CombatAreaManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Events;
+
+public class CombatAreaManager : MonoBehaviour
+{
+    public UnityEvent StartFight;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        StartFight.Invoke();
+        
+        var pm = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
+        if (pm)
+        {
+            pm.transform.position = transform.position ;
+            pm.transform.rotation = transform.rotation;
+            pm.currentCombatManager= transform.parent.GetComponent<CombatManager>();
+            pm.menu.SetActive(true);
+            pm.currentCombatManager.StartBattle();
+        }
+
+    }
+
+
+    
+}
