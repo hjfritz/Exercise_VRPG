7feec1309e59a2ae9c876661a93601b9238bd714
Katy Linn <katylinn@gmail.com>
2022-10-16 12:18:04 -0700

    poaching previous scene version and fixing level 1 scene

--- Assets/Scripts/CombatAreaManager.cs
+++ Assets/Scripts/CombatAreaManager.cs
@@ -15,11 +15,17 @@ public class CombatAreaManager : MonoBehaviour
     private Vector3 _teleportEnd;
     private bool _isTeleporting;
     private XROrigin _rig;
+    private PlayerManager _pm;
     private float _teleportTimer = 0f;
+
+    private void Start()
+    {
+        _rig = FindObjectOfType<XROrigin>();
+        _pm = FindObjectOfType<PlayerManager>();
+    }
 
     private void BeginTeleport()
     {
-        _rig = GameObject.Find("XR Origin").GetComponent<XROrigin>();
         _teleportStart = _rig.transform.position;
         _teleportEnd = transform.position;
         _isTeleporting = true;
@@ -44,34 +50,43 @@ public class CombatAreaManager : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        
-        
-        StartFight.Invoke();
-        
-        var pm = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
-        if (pm)
+        //bugfix - ontriggerenter was getting called multiple times during the fight by the colliders on the hands. 
+        //There is still an issue if the XR rig leaves and triggers again durring the battle, but that seems like
+        //something we can solve hen we disable locomotion
+        if (other.gameObject == _rig.gameObject)
         {
-            pm.transform.position = transform.position ;
-            pm.transform.rotation = transform.rotation;
-            pm.currentCombatManager= transform.parent.GetComponent<CombatManager>();
-            pm.menu.SetActive(true);
-            pm.currentCombatManager.StartBattle();
-            pm.FightStart.Invoke();
+            Debug.Log("XRRIg Triggered Enter");
+            StartFight.Invoke();
+        
+            if (_pm)
+            {
+                _pm.transform.position = transform.position ;
+                _pm.transform.rotation = transform.rotation;
+                _pm.currentCombatManager= transform.parent.GetComponent<CombatManager>();
+                //_pm.menu.SetActive(true);
+                _pm.currentCombatManager.StartBattle();
+                _pm.FightStart.Invoke();
+            }
+        
+            BeginTeleport();
         }
         
-        BeginTeleport();
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        var pm = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
-        if (pm)
+        //bugfix - ontriggerexit was getting triggered by hands
+        if (other.gameObject == _rig.gameObject)
         {
-
-            pm.currentCombatManager = null;
-            pm.menu.SetActive(false);
-            pm.FightEnd.Invoke();
+            Debug.Log("XRRIg Triggered Exit");
+            var pm = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
+            if (pm)
+            {
+                pm.currentCombatManager = null;
+                pm.menu.SetActive(false);
+                pm.FightEnd.Invoke();
+            }
         }
     }
     
