121b3106fe2a429e9480aadbcf383469de196615
hjfritz <fritzhj@gmail.com>
2022-10-16 22:56:03 -0700

    Bug fixes for demo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,6 +21,6 @@ public class GameManager : MonoBehaviour
 
     public static void OpenPortal()
     {
-        Debug.Log("Success!");
+        SceneManager.LoadScene(1);
     }
 }
