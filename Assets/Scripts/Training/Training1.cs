66fc15f6561313895afd6490ad8d6535193e2145
hjfritz <fritzhj@gmail.com>
2022-10-19 16:12:56 -0700

    Placed trainers and made sphere float to player

--- Assets/Scripts/Training/Training1.cs
+++ Assets/Scripts/Training/Training1.cs
@@ -11,9 +11,10 @@ namespace Training
 {
     public class Training1 : MonoBehaviour
     {
-        
-        [SerializeField] private GameObject orbA1;
         [SerializeField] private GameObject player;
+        [SerializeField] private GameObject orbA1;
+
+        [SerializeField] private float speed;
         
         //AudioClips
         [SerializeField] private AudioClip intro;
@@ -21,14 +22,15 @@ namespace Training
         [SerializeField] private AudioClip no;
         [SerializeField] private AudioClip training;
         [SerializeField] private AudioClip outro;
+        
 
         private GameObject teacher;
+        private GameObject currentOrb;
         private bool chosen = false;
         private Animator _animator;
         private bool actionComplete = false;
 
         private BattleAbility trainingAbility;
-
 
         private void Start()
         {
@@ -114,7 +116,7 @@ namespace Training
 
         IEnumerator Outro()
         {
-            Instantiate(orbA1, teacher.transform.position, quaternion.identity);
+            StartCoroutine(FloatSphereToPlayer());
             
             teacher.GetComponent<AudioSource>().PlayOneShot(outro);
             yield return new WaitUntil((() => teacher.GetComponent<AudioSource>().isPlaying == false));
@@ -122,6 +124,22 @@ namespace Training
             Destroy(teacher);
             player.GetComponent<LocomotionSwitch>().locomotionOn = true;
             
+        }
+        
+        IEnumerator FloatSphereToPlayer()
+        {
+            float elapsedTime = 0;
+            Vector3 startPos = teacher.transform.position + (Vector3.up * 1.5f);
+            Vector3 endPos = startPos + (((player.transform.position + (Vector3.up * 1.5f))- startPos) * .75f);
+
+            currentOrb = Instantiate(orbA1, startPos, Quaternion.identity);
+
+            while (elapsedTime < speed)
+            {
+                currentOrb.transform.position = Vector3.Lerp(startPos, endPos, elapsedTime / speed);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
